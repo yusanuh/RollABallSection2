e60ffad5992d83fafc24ea2e74d24d7cfd24df51
yusanuh <72392974+yusanuh@users.noreply.github.com>
2020-10-16 18:17:53 +0300

    Add files via upload

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(eulers: new Vector3(x: 15f, y: .2f, z: 92f) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forceMultiplier;
+    private float moveHorizontal;
+    private float moveVertical;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(x: 0, y: 5f, z: 0, ForceMode.Impulse);
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * forceMultiplier;
+        moveVertical = Input.GetAxis("Vertical") * forceMultiplier;
+        rigidbody.AddForce(x: moveHorizontal, y: 0, z: moveVertical, ForceMode.Acceleration);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("coin"))
+        {
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+}
